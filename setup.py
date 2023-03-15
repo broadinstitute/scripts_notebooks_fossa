ef3aabfd6408175b18ca62c064c8b868e9799c9f
fefossa <fefossa@gmail.com>
2023-03-15 14:04:35 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='utils',
+      version='1.0',
+      description='Scripts to run PhD analysis.',
+      author='Fernanda Fossa',
+      author_email='fefossa@gmail.com',
+      url='https://github.com/broadinstitute/scripts_notebooks_fossa',
+     )
