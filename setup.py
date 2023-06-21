b174979d9069521cc8d50c77c88e7c880ff3716a
fefossa <fefossa@gmail.com>
2023-06-21 07:33:07 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-setup(name='utils',
+setup(name='scripts_fossa',
       version='1.0',
       description='Scripts to run PhD analysis.',
       author='Fernanda Fossa',
       author_email='fefossa@gmail.com',
       url='https://github.com/broadinstitute/scripts_notebooks_fossa',
+      py_modules=[],
      )
