34f15c280d9e803725dbe74e08dee1a0a89d8958
Fernanda Fossa <fefossa@gmail.com>
2023-11-13 07:03:17 -0300

    update umap, tsne and pca scripts

--- pycombat_umap/combat_util.py
+++ pycombat_umap/combat_util.py
@@ -51,7 +51,7 @@ def pycombat_generator(df, well_column = 'Metadata_Well', batch_column = 'Metada
     
     return df_corr
 
-def generate_x_y_umap(df, n_neighbors = 5, min_dist = 0.1, n_components = 2, metric='cosine', noncanonical_features=False):
+def generate_x_y_umap(df, n_neighbors = 5, min_dist = 0.1, n_components = 2, metric='cosine', noncanonical_features=False, iterate=False, number_runs=None):
     """
     This function will generate an X and y from the inputed dataframe and fit_transform based on the parameters specified
     *df (DataFrame): df containing the features as columns and samples as rows;
@@ -61,7 +61,7 @@ def generate_x_y_umap(df, n_neighbors = 5, min_dist = 0.1, n_components = 2, met
         - This means that low values of min_dist will result in clumpier embeddings. 
     *col (str): 
     """
-
+    import numpy as np
     meta = pycytominer.cyto_utils.features.infer_cp_features(df, metadata=True)
     if noncanonical_features:
         feat = [x for x in df.columns.tolist() if x not in meta]
@@ -76,14 +76,37 @@ def generate_x_y_umap(df, n_neighbors = 5, min_dist = 0.1, n_components = 2, met
     projections = umap_2d.transform(X)
     columns = [str(x) for x in range(0, n_components)]
 
-    proj_df = pd.DataFrame(data=projections, columns=columns)
+    if iterate:
+        # Create an empty list to store UMAP embeddings
+        umap_embeddings = []
 
-    umap_df = pd.concat([proj_df, y], axis='columns')
+        # Number of UMAP runs
+        num_runs = number_runs
+
+        for _ in range(num_runs):
+            umap_2d = UMAP(n_neighbors=n_neighbors, min_dist=min_dist, n_components=n_components, init='random', random_state=0, metric=metric)
+            embedding_umap = umap_2d.fit_transform(X)  # Replace your_data with your actual data
+            umap_embeddings.append(embedding_umap)
+        
+        # Convert the list to a NumPy array for easier manipulation
+        umap_results_array = np.array(umap_embeddings)
+
+        # Calculate mean and standard deviation along each dimension
+        mean_embedding = np.mean(umap_results_array, axis=0)
+        std_embedding = np.std(umap_results_array, axis=0)
+        
+        # Dataframes
+        error_df = pd.DataFrame(data=std_embedding, columns=['x_err', 'y_err'])
+        proj_df = pd.DataFrame(data=mean_embedding, columns=columns)
+        umap_df = pd.concat([proj_df, error_df, y], axis='columns')
+    else:
+        proj_df = pd.DataFrame(data=projections, columns=columns)
+        umap_df = pd.concat([proj_df, y], axis='columns')
 
     return umap_df
 
 def plot_umap(df_plot, color_col, hover_cols, split_df = False, split_column = None, np = None, discrete = False, size=False, size_col = None, umap_param=False, neighbor=None, mindist=None, compound_color=False, time_color=False, dili_color=False,
-              x="0", y="1"):
+              x="0", y="1", symbol="symbol", error_x=None, error_y=None):
     """
     Plot UMAP components to visualize results using plotly scatter function. Various parameters are optional to choose and customize the plots.
     
@@ -154,7 +177,8 @@ def plot_umap(df_plot, color_col, hover_cols, split_df = False, split_column = N
             hover_data=hover_cols,
             color_continuous_scale=px.colors.sequential.Bluered,
             size='Metadata_size',
-            color_discrete_map=color_discrete
+            color_discrete_map=color_discrete,
+            error_x=error_x, error_y=error_y
             )
         else:
             fig = px.scatter(
@@ -171,7 +195,8 @@ def plot_umap(df_plot, color_col, hover_cols, split_df = False, split_column = N
         color='colors_plot_col',
         hover_data=hover_cols,
         color_continuous_scale=px.colors.sequential.Bluered,
-        color_discrete_sequence=color_sequence
+        color_discrete_sequence=color_sequence,
+        error_x=error_x, error_y=error_y
         )
         fig.update_traces(marker={'size': 12})
 
@@ -448,3 +473,228 @@ def col_generator(df, cols_to_join = ['Metadata_Compound', 'Metadata_Concentrati
     print("Names of the compounds + concentration: ",  df[new_col].unique())
 
     return df, new_col
+
+def tsne_generator(df, perplexity=40, n_components = 2, metric='cosine', noncanonical_features=False, iterate=False, number_runs=None):
+    """ 
+    
+    """
+    from sklearn.manifold import TSNE
+    import numpy as np
+
+    meta = pycytominer.cyto_utils.features.infer_cp_features(df, metadata=True)
+    if noncanonical_features:
+        feat = [x for x in df.columns.tolist() if x not in meta]
+    else:
+        feat = pycytominer.cyto_utils.features.infer_cp_features(df, metadata=False)
+        
+    X = pd.DataFrame(df, columns=feat)
+    y = pd.DataFrame(df, columns=meta)
+
+    tsne = TSNE(n_components=n_components, random_state=42, perplexity=perplexity)
+    X_tsne = tsne.fit_transform(X)
+    print(tsne.kl_divergence_)
+
+    columns = [str(x) for x in range(0, n_components)]
+
+    if iterate:
+        # Create an empty list to store UMAP embeddings
+        tsne_embeddings = []
+
+        # Number of UMAP runs
+        num_runs = number_runs
+
+        for _ in range(num_runs):
+            tsne = TSNE(n_components=n_components, random_state=42, perplexity=perplexity)
+            embedding_tsne = tsne.fit_transform(X)  # Replace your_data with your actual data
+            print(tsne.kl_divergence_)
+            tsne_embeddings.append(embedding_tsne)
+        
+        # Convert the list to a NumPy array for easier manipulation
+        tsne_results_array = np.array(tsne_embeddings)
+
+        # Calculate mean and standard deviation along each dimension
+        mean_embedding = np.mean(tsne_results_array, axis=0)
+        std_embedding = np.std(tsne_results_array, axis=0)
+        
+        # Dataframes
+        error_df = pd.DataFrame(data=std_embedding, columns=['x_err', 'y_err'])
+        proj_df = pd.DataFrame(data=mean_embedding, columns=columns)
+        tsne_df = pd.concat([proj_df, error_df, y], axis='columns')
+    else:
+        proj_df = pd.DataFrame(data=X_tsne, columns=columns)
+        tsne_df = pd.concat([proj_df, y], axis='columns')
+
+    return X, tsne_df
+
+def tsne_divergence(X_train):
+
+    import numpy as np
+    from sklearn.manifold import TSNE
+
+    perplexity = np.arange(5, 80, 5)
+    divergence = []
+
+    for i in perplexity:
+        model = TSNE(n_components=2, init="pca", perplexity=i)
+        reduced = model.fit_transform(X_train)
+        divergence.append(model.kl_divergence_)
+    fig = px.line(x=perplexity, y=divergence, markers=True)
+    fig.update_layout(xaxis_title="Perplexity Values", yaxis_title="Divergence")
+    fig.update_traces(line_color="red", line_width=1)
+    fig.show()
+
+def plot_tsne(df_plot, color_col, hover_cols, split_df = False, split_column = None, 
+              np = None, discrete = False, size=False, size_col = None, umap_param=False, 
+              neighbor=None, mindist=None, time_color=False,
+              x="0", y="1", label_legend = "", title_plot="",
+              symbol=False, symbol_col=None, symbol_list=None,
+              error_x=None, error_y=None, dili_color=False):
+
+    if split_df:
+        df = df_plot[df_plot[split_column] == np].reset_index()
+        title_plot = np
+        if umap_param:
+            title_plot = np + ' N: ' + str(neighbor) + ' M: ' + str(mindist)
+    else:
+        df = df_plot.copy()
+        # title_plot = 'Labeled by '+ color_col
+        # if umap_param:
+        #     title_plot = 'N: ' + str(neighbor) + ' M: ' + str(mindist)
+    #if the color_col is a int, sort the columns by value
+    if df[color_col].map(type).eq(int).any():
+        df.sort_values(color_col, inplace=True)
+
+    if discrete:
+        df['colors_plot_col'] = df[color_col].astype(str)
+        color_sequence = px.colors.sequential.Plasma
+        if time_color:
+            color_sequence = ['royalblue', 'green','orange','red']
+    elif dili_color:
+        df['colors_plot_col'] = df[color_col]
+        color_discrete = {"Aspirin": 'rgb(229, 134, 6)', 'Amiodarone': 'rgb(93, 105, 177)', "Cyclophosphamide": 'rgb(82, 188, 163)', "Etoposide": 'rgb(153, 201, 69)',
+                  "Vehicle-ETP":'rgb(204, 97, 176)', "Non-treated":'rgb(36, 121, 108)', "Lovastatin":'rgb(218, 165, 27)', "Orphenadrine":'rgb(47, 138, 196)',
+                  "Tetracycline":'rgb(118, 78, 159)', "DMSO":'rgb(237, 100, 90)', "Lactose":'rgb(165, 170, 153)'}
+        color_sequence = px.colors.qualitative.Vivid
+    else:
+        df['colors_plot_col'] = df[color_col]
+        color_sequence = px.colors.qualitative.Vivid
+
+    if size:
+        df['Metadata_size'] = df[size_col].astype(int)
+        if not time_color:
+            df.sort_values('Metadata_size', inplace=True)
+        if symbol:
+            fig = px.scatter(
+            df, x=x, y=y,
+            color='colors_plot_col',
+            hover_data=hover_cols,
+            color_continuous_scale=px.colors.sequential.Bluered,
+            size='Metadata_size',
+            color_discrete_sequence=color_sequence,
+            symbol=symbol_col,
+            symbol_sequence=symbol_list
+            )
+        if dili_color:
+            fig = px.scatter(
+            df, x=x, y=y,
+            color='colors_plot_col',
+            hover_data=hover_cols,
+            color_continuous_scale=px.colors.sequential.Bluered,
+            size='Metadata_size',
+            color_discrete_map=color_discrete,
+            error_x=error_x, error_y=error_y
+            )
+        else:
+            fig = px.scatter(
+            df, x=x, y=y,
+            color='colors_plot_col',
+            hover_data=hover_cols,
+            color_continuous_scale=px.colors.sequential.Bluered,
+            size='Metadata_size',
+            color_discrete_sequence=color_sequence,
+            error_x=error_x, error_y=error_y
+            )
+    else:
+        fig = px.scatter(
+        df, x=x, y=y,
+        color='colors_plot_col',
+        hover_data=hover_cols,
+        color_continuous_scale=px.colors.sequential.Bluered,
+        color_discrete_sequence=color_sequence,
+        error_x=error_x, error_y=error_y
+        )
+        # fig.update_traces(marker={'size': 12})
+
+    fig.update_layout(
+        font=dict(
+        size=18),
+        legend_title=label_legend,
+        title=title_plot,
+        autosize=False,
+        width=900,
+        height=700,
+        margin=dict(
+            l=50,
+            r=50,
+            b=20,
+            t=50,
+            pad=4
+        ),
+        xaxis_title="TSNE X",
+        yaxis_title="TSNE Y"
+        )
+
+    # fig.show("notebook")
+    config = {
+    'toImageButtonOptions': {
+        'format': 'svg', # one of png, svg, jpeg, webp
+        'filename': 'custom_image',
+        'height': 500,
+        'width': 700,
+        'scale':6 # Multiply title/legend/axis/canvas sizes by this factor
+    }
+    }
+
+    return fig.show(config=config)
+
+def generate_pca(df, n_components = 2, noncanonical_features=False):
+    """
+    This function will generate an X and y from the inputed dataframe and fit_transform based on the parameters specified
+    *df (DataFrame): df containing the features as columns and samples as rows;
+    *n_neighbors (int): number of neighbors for UMAP to calculate on 
+        - The size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation.
+    *min_dist (int): The min_dist parameter controls how tightly UMAP is allowed to pack points together. Minimum distance apart that points are allowed to be in the low dimensional representation. 
+        - This means that low values of min_dist will result in clumpier embeddings. 
+    *col (str): 
+    """
+    from sklearn.decomposition import PCA
+
+    meta = pycytominer.cyto_utils.features.infer_cp_features(df, metadata=True)
+    if noncanonical_features:
+        feat = [x for x in df.columns.tolist() if x not in meta]
+    else:
+        feat = pycytominer.cyto_utils.features.infer_cp_features(df, metadata=False)
+        
+    X = pd.DataFrame(df, columns=feat)
+    y = pd.DataFrame(df, columns=meta)
+
+    pca = PCA(n_components=n_components)
+
+    # Fit and transform the data
+    pca_result = pca.fit_transform(X)
+
+    # Access the principal components and explained variance ratio
+    components = pca.components_
+    explained_variance_ratio = pca.explained_variance_ratio_
+
+    # Print the results
+    print(f"Explained Variance Ratio: {explained_variance_ratio}")
+
+
+    columns = [str(x) for x in range(0, n_components)]
+
+    proj_df = pd.DataFrame(data=pca_result, columns=columns)
+
+    pca_df = pd.concat([proj_df, y], axis='columns')
+
+    return pca_df
