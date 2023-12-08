c48990aa3aa14b8f831d36e2e00b64439ebcd895
Fernanda Fossa <fefossa@gmail.com>
2023-12-08 06:34:48 -0300

    z test, anova, and plot with external pvalue

--- individual_feature_and_statistics/feat_stat.py
+++ individual_feature_and_statistics/feat_stat.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import normaltest
 from statannotations.Annotator import Annotator
+from statannotations.stats.StatTest import StatTest
 
 def col_generator(df, cols_to_join = ['Metadata_Compound', 'Metadata_Concentration']):
     """
@@ -157,3 +158,137 @@ def plot_with_markers(df, feature, x, palette_boxplot=None, order_to_plot = [], 
         annot.configure(test=stat_test, text_format='star', loc='inside', verbose=2).apply_test().annotate()
     g.set_xticklabels(new_labels, fontsize=15)
     plt.show()
+
+
+def plot_with_anova(df, feature, x, palette_boxplot=None, order_to_plot = [], hue_order_boxplot = [], 
+                  pairs_stat = [], show_hist=False, rotation=75, 
+                  new_labels=[], ylabel="", hue_col="Metadata_Time", title_plot=None,
+                  title_legend='Time (days)',
+                  set_lim=False, xlim=None, ylim=None,
+                  col_groupby=None, category_mapping=None,
+                  perform_stat_test=True):
+    """
+    
+    """    
+    from scipy.stats import f_oneway
+    # Required descriptors for annotate
+    custom_long_name = 'One-way ANOVA statistical test'
+    custom_short_name = 'One-way ANOVA'
+    custom_func = f_oneway
+    custom_test = StatTest(custom_func, custom_long_name, custom_short_name)
+    
+    sns.set(font_scale=1.7)
+    sns.set_style("dark")
+    g=sns.catplot(data=df,
+                  x=x,
+                  y=feature,
+                  kind="box",
+                  legend=False,
+                  height=7,
+                  aspect=1.5,
+                  palette=palette_boxplot,
+                  boxprops={'alpha': 0.6},
+                  order=order_to_plot,
+                  saturation=1
+                  )
+
+    # Create the strip plot
+    for category, group_data in df.groupby(col_groupby):
+        properties = category_mapping.get(category, {"marker": "o", "color": "black"})
+        marker = properties["marker"]
+        color = properties["color"]
+        plt.scatter(group_data[x], group_data[feature], label=category, marker=marker, 
+                    color="none",  # Set facecolor to "none" for no fill
+            edgecolor=color,  # Use the specified color for marker edges
+                    )
+    plt.legend([],[], frameon=False)
+    g.add_legend(title='')
+    g.set(xlabel=None, title=title_plot, ylabel=ylabel)
+    # Customize the appearance
+    if set_lim:
+        g.ax.set_ylim(xlim,ylim)
+    plt.xticks(rotation=rotation)
+    plt.legend([],[], frameon=False)
+    # g.add_legend(title=title_legend)
+
+    if perform_stat_test:
+        annot = Annotator(g.ax, pairs_stat, 
+                            data=df, x=x, y=feature, order=order_to_plot)
+        annot.reset_configuration()
+        annot.new_plot(g.ax, pairs_stat, data=df, x=x, y=feature, order=order_to_plot)
+        annot.configure(test=custom_test, text_format='star', loc='inside', verbose=2).apply_test().annotate()
+    g.set_xticklabels(new_labels, fontsize=15)
+    plt.show()
+
+def plot_pvalue_calculated_elsewhere(df, feature, x, palette_boxplot=None, order_to_plot = [], hue_order_boxplot = [], 
+                  pairs_stat = [], show_hist=False, rotation=75, 
+                  new_labels=[], ylabel="", hue_col="Metadata_Time", title_plot=None,
+                  title_legend='Time (days)',
+                  set_lim=False, xlim=None, ylim=None,
+                  col_groupby=None, category_mapping=None,
+                  perform_stat_test=True,
+                  p_values=None):
+    """
+    
+    """ 
+    
+    sns.set(font_scale=1.7)
+    sns.set_style("dark")
+    g=sns.catplot(data=df,
+                  x=x,
+                  y=feature,
+                  kind="box",
+                  legend=False,
+                  height=7,
+                  aspect=1.5,
+                  palette=palette_boxplot,
+                  boxprops={'alpha': 0.6},
+                  order=order_to_plot,
+                  saturation=1
+                  )
+
+    # Create the strip plot
+    for category, group_data in df.groupby(col_groupby):
+        properties = category_mapping.get(category, {"marker": "o", "color": "black"})
+        marker = properties["marker"]
+        color = properties["color"]
+        plt.scatter(group_data[x], group_data[feature], label=category, marker=marker, 
+                    color="none",  # Set facecolor to "none" for no fill
+            edgecolor=color,  # Use the specified color for marker edges
+                    )
+    plt.legend([],[], frameon=False)
+    g.add_legend(title='')
+    g.set(xlabel=None, title=title_plot, ylabel=ylabel)
+    # Customize the appearance
+    if set_lim:
+        g.ax.set_ylim(xlim,ylim)
+    plt.xticks(rotation=rotation)
+    plt.legend([],[], frameon=False)
+    # g.add_legend(title=title_legend)
+
+    if perform_stat_test:
+        annot = Annotator(g.ax, pairs_stat, 
+                            data=df, x=x, y=feature, order=order_to_plot)
+        annot.reset_configuration()
+        annot.new_plot(g.ax, pairs_stat, data=df, x=x, y=feature, order=order_to_plot)
+        annot.configure(text_format="star", loc="inside")
+        annot.set_pvalues_and_annotate(p_values)
+    g.set_xticklabels(new_labels, fontsize=15)
+    plt.show()
+
+
+def z_test_pairs(df, feature, pairs, label_column):
+    """
+    """
+    from statsmodels.stats.weightstats import ztest
+
+    pvalues_list = []
+    for p in pairs:
+        ztest_value, p_valor = ztest(x1=df.query(f"{label_column} in '{p[0]}'").reset_index()[feature], x2=df.query(f"{label_column} in '{p[1]}'").reset_index()[feature])
+        if p_valor<.05:
+            print(f'{p[0]} x {p[1]}: p-value {p_valor}, REJECT null hypothesis')
+        else:
+            print(f"{p[0]} x {p[1]}: p-value {p_valor}, failed to reject null hypothesis")
+        pvalues_list.append(p_valor)
+
+    return pvalues_list
